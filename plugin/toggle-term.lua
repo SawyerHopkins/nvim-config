fdfcf3a5ce6aac9e761f30f10e08349f18736e93
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2026-08-24 08:10:11 +0000

    toggle term cleanup

--- plugin/toggle-term.lua
+++ plugin/toggle-term.lua
@@ -10,9 +10,10 @@ vim.pack.add({ 'https://github.com/akinsho/toggleterm.nvim' }, { confirm = false
 
 local tt = require('toggleterm')
 tt.setup({
-  shell = "/bin/bash",
+  shell = vim.o.shell,
   direction = 'vertical',
   open_mapping = { [[<c-\>]] },
+  persist_size = false,
   size = function(term)
     if term.direction == "horizontal" then
       return 15
@@ -37,7 +38,22 @@ vim.api.nvim_create_autocmd('TermOpen', {
 })
 
 local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
 
 function _lazygit_toggle()
   lazygit:toggle()
